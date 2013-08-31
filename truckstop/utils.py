137e9c1e28716e9c5360171198736e34e6e3556a
Andrew Gwozdziewycz <git@apgwoz.com>
2013-08-30 20:31:29 -0400

    * remove some print statements

--- truckstop/utils.py
+++ truckstop/utils.py
@@ -8,19 +8,15 @@ def param_validator(**specs):
     def dec(func):
         @wraps(func)
         def orator():
-            print "query is: ", request.query
-
             kwargs = {}
             for n, s in specs.iteritems():
                 try:
-                    print "param_validator", n, getattr(request.query, n)
                     v = s[0](getattr(request.query, n))
                 except:
                     if s[1]:
                         return {'error': s[1]}
                 else:
                     kwargs[n] = v
-                    print kwargs
             try:
                 return func(**kwargs)
             except ValueError, e:
