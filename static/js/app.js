e4b32911e5488c57ccc328cd5d2f9d6d26514bf0
Andrew Gwozdziewycz <git@apgwoz.com>
2013-08-31 09:46:29 -0400

    * lots of interaction changes. looks good enough to ship

--- static/js/app.js
+++ static/js/app.js
@@ -8,12 +8,14 @@ if (!String.prototype.encodeHTML) {
 }
 $(function() {
   var DEFAULT_POSITION = [37.783333, -122.416667];
+  var DEFAULT_ICON = new L.Icon.Default();
   var CURRENT_LOCATION = [DEFAULT_POSITION[0], DEFAULT_POSITION[1]];
+  var CURRENT_LOCATION_MARKER;
   var MARKERS = {};
   var MAP = L.map('map').setView(DEFAULT_POSITION, 13);
-  var RESULT_TEMPLATE = Mustache.compile('<h2>Search Results</h2>' + 
+  var RESULT_TEMPLATE = Mustache.compile('<span class="clear-button">X</span><h2>Search Results</h2>' + 
 '  {{#venues}}' +
-'<dl data-id="{{ObjectID}}">' + 
+'<dl id="anchor-{{ObjectID}}" data-id="{{ObjectID}}">' + 
 '    <dt><span class="distance">{{ distance_desc }}</span><span class="name">{{ Applicant }}</span></dt>' +
 '    <dd><span class="address">{{ Address }}</span>' +
 '     <p><strong>Serves:</strong> {{ FoodItems }}</p>' +
@@ -23,6 +25,15 @@ $(function() {
 '   <h2>No results found!</h2>' +
 '  {{/venues}}');
 
+  var HIGHLIGHT_ICON = L.icon({
+      iconUrl: '/static/img/active-marker-icon.png',
+      iconRetinaUrl: '/static/img/active-marker-icon@2x.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41],
+      shadowUrl: '/static/img/marker-shadow.png'
+  });
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -37,6 +48,16 @@ $(function() {
       console.log(pbits[0] + " = " + pbits[1]);
     }
     return result;
+  };
+
+  var resetEverything = function() {
+    for (var m in MARKERS) {
+      MAP.removeLayer(MARKERS[m]);
+    };
+    MARKERS = {};
+    $('#map').css({'height': '500px'});
+    MAP.invalidateSize();
+    $('.result-list').css({'display': 'none'});
   };
 
   var getPosition = function(cb) {
@@ -64,16 +85,37 @@ $(function() {
       req.fail(ecb);
     }
   };
+
+
+
+  $(document).on('click', '.clear-button', function(e) {
+    $('.search input').val('');
+    resetEverything();
+  });
   
   // center the map on our current position
   getPosition(function(position) {
     MAP.setView(position, 13);
-    CURRENT_POSITION = position;
+    CURRENT_LOCATION = position;
+
+    var here = L.icon({
+        iconUrl: '/static/img/here-marker-icon.png',
+        iconRetinaUrl: '/static/img/here-marker-icon@2x.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowUrl: '/static/img/marker-shadow.png',
+        shadowSize: [41, 41]
+    });
+
+    CURRENT_LOCATION_MARKER = L.marker(CURRENT_LOCATION, {icon: here}).addTo(MAP);
+
   });
 
   $('.search input').keyup(function(event) {
     if (event.which == 13) {
       event.preventDefault();
+      resetEverything();
       var query = $('.search input').val();
 
       getSearchResults(query, function(results) {
@@ -82,6 +124,19 @@ $(function() {
           for (var i = 0; i < results.venues.length; i++) {
             var venue = results.venues[i];
             MARKERS[venue['ObjectID']] = L.marker([parseFloat(venue['Latitude']), parseFloat(venue['Longitude'])]).addTo(MAP);
+            MARKERS[venue['ObjectID']].on('mouseover', (function (v) {
+              return function(e) {
+                 MARKERS[v['ObjectID']].setIcon(HIGHLIGHT_ICON);
+                 var off = $('#anchor-' + v['ObjectID']).offset();
+                 $(window).scrollTop(off.top - 220);
+                 $('#anchor-' + v['ObjectID']).addClass('hover-state');
+              }})(venue));
+            MARKERS[venue['ObjectID']].on('mouseout', (function (v) {
+              return function(e) {
+                 MARKERS[v['ObjectID']].setIcon(DEFAULT_ICON);
+                 $('#anchor-' + v['ObjectID']).removeClass('hover-state');
+              }})(venue));
+
           }
 
           $('#map').css({'height': '200px'});
@@ -98,16 +153,16 @@ $(function() {
     $(this).addClass('hover-state');
     var marker = MARKERS[$(this).data('id')];
     MAP.panTo(marker.getLatLng());
-
+    MARKERS[$(this).data('id')].setIcon(HIGHLIGHT_ICON);
   });
   $(document).on('mouseout', '.result-list dl', function(event) {
     $(this).removeClass('hover-state');
+    MARKERS[$(this).data('id')].setIcon(DEFAULT_ICON);
   });
 
   // On scroll when result list is available, fix map to the top of the screen.
   $(window).scroll(function() {
     if ($('.result-list:visible')) {
-      console.log($(window).scrollTop());
       if ($(window).scrollTop() > 58) {
         $('#map').css({'top': ($(window).scrollTop() - 58) + 'px'}).addClass('map-scrolling');
       }
